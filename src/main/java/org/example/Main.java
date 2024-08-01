ed17703f38a8210a0483e8b4c52355720a1051c1
Your Name <mikhailmakarov92@gmail.com>
2024-08-01 14:46:48 +0400

    Созданно два класса

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import static org.example.PairUtil.concatOrSum;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pair<Integer, Integer> intPair = new Pair<>(3, 5);
+        Pair<Double, Double> doublePair = new Pair<>(2.5, 3.5);
+        Pair<String, String> stringPair = new Pair<>("Привет", "Олег");
+
+        System.out.println("Сумма целых чисел: " + concatOrSum(intPair));
+        System.out.println("Сумма дробных числе: " + concatOrSum(doublePair));
+        System.out.println("Текст из объединенных строк: " + concatOrSum(stringPair));
     }
 }
